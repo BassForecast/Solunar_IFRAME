94d25d49dc2dbeaa57dabbc50d829e03c464f502
Raviteja Vakalapudi <raviteja.vakalapudi@mutualmobile.com>
2019-01-16 16:13:21 +0530

    Using new api for BFR

--- src/constants/BassForecastInfo.js
+++ src/constants/BassForecastInfo.js
@@ -1,3 +1,4 @@
 export const BF_SOLUNAR_API = "https://4stknmloug.execute-api.us-east-1.amazonaws.com/prod/solunar-lib";
-export const BFR_API = "https://virz2o3esc.execute-api.us-east-1.amazonaws.com/prod/v1";
-export const BFR_API_KEY = "e4iAsWEXdB2YnR0GVJYPI9kuKXUVU1LHaBBerkOD";
+export const BFR_API = "https://r3hdu19hif.execute-api.us-east-2.amazonaws.com/Prod/bfr";
+export const BFR_API_KEY = "kMavcyx7rY2GPx5MeBftI6dJLNFu3rBn5dMbC469";
+export const BFR_REQUEST_KEY = "S2jtsbRNhg26";
